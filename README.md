751dfd5289364ff04d4ee5a2bde5d62fee519fbb
Elizabeth Franklin <elizabethwfranklin@gmail.com>
2025-08-05 17:36:16 -0400

    Deviation Commit

--- README.md
+++ README.md
@@ -6,17 +6,24 @@
 ### Fair Warning
 This course is outdated and I've had to do a decent amount of research to bring my tech up-to-date. That also means that some of the lines may not be in the most updated syntax. 😒 But I'm working through it as an exercise of troubleshooting and working with a legacy codebase. 💁‍♀️
 
+### Update to the Warning
+I've now completely deviated from the course because it was so incredibly outdated. I've left the code from the course commented at the bottom, but I'm working straight from [MongoDB's docs](https://www.mongodb.com/docs/drivers/node/) now.
+
 ## Required Tech
 (Outside of your standard IDE, CLI, etc.)
 - [MongoDB](https://www.mongodb.com/)
 - [Node.js](https://nodejs.org/)
 
 ## Install Order
-1. ```npm init```
-2. ```npm install```
-3. ```npm i mongodb```
-4. ```npm install dotenv```
+1. `javascript npm init`
+2. `npm install`
+3. `npm i mongodb`
+4. `npm install dotenv`
     a. Only necessary if using environment variables (like me).
 
 ## Final Verdict
 TBD
+
+---
+
+last updated: 8/5/25
--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-require('dotenv').config();
+require("dotenv").config();
 
 // The MongoClient will be required to connect
 const MongoClient = require("mongodb").MongoClient;
@@ -6,21 +6,72 @@ const MongoClient = require("mongodb").MongoClient;
 // Specify the URL of the MongoDB
 const dbUrl = process.env.DB_PATH;
 
-console.log(dbUrl);
+// Indicate which URL MongoClient should use
+const client = new MongoClient(dbUrl);
 
-let database;
+// List of items to add
+const newData = [
+    {
+        insertOne: {
+            document: {
+                Name: "Gus",
+                Age: 11,
+            },
+        },
+    },
+    {
+        insertOne: {
+            document: {
+                Name: "Lily",
+                Age: 7,
+            },
+        },
+    },
+    {
+        insertOne: {
+            document: {
+                Name: "Hudson",
+                Age: 6,
+            },
+        },
+    },
+];
 
-const databaseName = "Fullstack Web Mastery";
+// Create a function to access the DB
+let MyApp = async () => {
+    try {
+        await client.connect();
+        // Set the database (Project) name
+        const database = client.db("fullstack-web-mastery");
+        // Set the collection (Cluster) name
+        const allData = database.collection("fwm-data");
 
-// Connect to the appropriate URL
-// Once connected, take in an error and the client
-MongoClient.connect(dbUrl, (error, client) => {
+        const queriedData = await allData;
+        console.log({ queriedData });
 
-    // If there's an error, stop processing and return the error
-    if (error) return console.log(error);
+        let insertedData = await allData.bulkWrite(newData);
+        const updatedData = await allData.find({}).toArray();
+        console.log(updatedData);
+    } catch (error) {
+        console.log({ error });
+    } finally {
+        client.close();
+    }
+};
 
-    // Pull the data from the database
-    database = client.db(databaseName);
-    console.log(`database: ${databaseName}`);
-    console.log(`DB URL: ${dbUrl}`);
-});
+MyApp();
+
+// let database;
+
+// // Connect to the appropriate URL
+// // Once connected, take in an error and the client
+// MongoClient.connect(dbUrl, (error, client) => {
+
+//     // If there's an error, stop processing and return the error
+//     if (error) return console.log(error);
+
+//     // Pull the data from the database
+//     database = client.db(databaseName);
+//     console.log(`database: ${databaseName}`);
+//     console.log(`DB URL: ${dbUrl}`);
+// });
